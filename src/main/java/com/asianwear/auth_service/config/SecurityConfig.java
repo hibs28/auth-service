0fb2956618705b247fdfb68abbaad3f07b828e62
Hibah Abid <hibah28@live.co.uk>
2025-02-17 17:24:23 +0000

    Add spring-security dependency to hash the password before adding to the db. Add jpa repository for User

---
+++ src/main/java/com/asianwear/auth_service/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.asianwear.auth_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+// passwords to be hashed before adding to database
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
